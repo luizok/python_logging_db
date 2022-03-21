6cf4090b842abc2b15c3a120b0344d2f1b0ae97b
Luiz Felipe <luizfelipesmaciel@gmail.com>
2022-03-20 21:31:55 -0300

    Execuçao em funçao

--- src/log/__init__.py
+++ src/log/__init__.py
@@ -4,37 +4,57 @@ import logging
 import src.log.dbloggers as dblog
 
 
-fmtr = logging.Formatter(
-    fmt='%(asctime)s.%(msecs).0f [%(levelname)s] - %(name)s.%(funcName)s (%(lineno)d) - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
-
-h1 = logging.FileHandler('logs/info.log', mode='w')
-h2 = logging.FileHandler('logs/error.log', mode='w')
-h3 = logging.StreamHandler(stream=sys.stdout)
-h4 = dblog.SQLiteLoggingHandler('error_log', 'errordb.db', **{'check_same_thread': False})
-h5 = dblog.MYSQLLoggingHandler('error_log', **{
-    'host': 'localhost',
-    'port': 3306,
-    'database': 'logs',
-    'user': 'user',
-    'password': 'senha',
-    'autocommit': True
-})
-
-h2.setLevel(logging.ERROR)
-h4.setLevel(logging.ERROR)
-h5.setLevel(logging.ERROR)
-
-handlers = [h1, h2, h3, h4, h5]
-
-for i, h in enumerate(handlers):
-    handlers[i].setFormatter(fmtr)
-
+handlers = []
 initialized = set()
+_called = False
+
+
+def init():
+
+    global handlers, _called
+
+    fmtr = logging.Formatter(
+        fmt='%(asctime)s.%(msecs).0f [%(levelname)s] - %(name)s.%(funcName)s (%(lineno)d) - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    hdlrs = [
+        logging.FileHandler('logs/info.log', mode='w'),
+        logging.FileHandler('logs/error.log', mode='w'),
+        logging.StreamHandler(stream=sys.stdout),
+        dblog.SQLiteLoggingHandler(
+            'error_log', 'errordb.db',
+            **{'check_same_thread': False}
+        ),
+    ]
+    # h5 = dblog.MYSQLLoggingHandler('error_log', **{
+    #     'host': 'localhost',
+    #     'port': 3306,
+    #     'database': 'logs',
+    #     'user': 'user',
+    #     'password': 'senha',
+    #     'autocommit': True
+    # })
+
+    # h5.setLevel(logging.ERROR)
+
+    for i, h in enumerate(hdlrs):
+        hdlrs[i].setLevel(logging.INFO)
+        hdlrs[i].setFormatter(fmtr)
+
+    hdlrs[1].setLevel(logging.ERROR)
+    hdlrs[3].setLevel(logging.ERROR)
+
+    handlers = hdlrs
 
 
 def get_logger(*names):
+
+    global handlers, initialized, _called
+
+    if not _called:
+        init()
+        _called = False
 
     logger = logging.getLogger('.'.join(names))
     if logger.name in initialized:
